3e7cb427460d28e33a27c0bf7db4d9b9e9396394
Kai Waldrant <kai.waldrant@outlook.com>
2025-12-02 23:43:27 +0100

    WIP js generate password

--- js/script.js
+++ js/script.js
@@ -1,6 +1,30 @@
 document.addEventListener("DOMContentLoaded", () => {
   const slider = document.getElementById("char-length");
   const charLengthValue = document.getElementById("char-length-Value");
+  const generateBtn = document.getElementById("generate-btn");
+  const arrowRight = document.getElementById("arrow-right");
+
+  let options = {};
+
+  const getOptions = () => {
+    document.querySelectorAll(".options__checkbox").forEach((item) => {
+      if (item.checked === true) {
+        options[item.id] = true;
+      } else if (item.checked === false) {
+        options[item.id] = false;
+      }
+    });
+  };
+
+  const generatePassword = (length, options) => {
+    getOptions();
+  };
+
+  generateBtn.addEventListener("click", () => {
+    const length = slider.value;
+    const password = generatePassword(length);
+    document.getElementById("password").textContent = password;
+  });
 
   // Calculate slider value in percentage to create background of slider
   const calcValue = () => {
@@ -15,4 +39,16 @@ document.addEventListener("DOMContentLoaded", () => {
   });
   // To make the background of the slider visible on page reload
   calcValue();
+
+  // Change arrow image on hover of generate button
+
+  generateBtn.addEventListener("mouseover", () => {
+    arrowRight.src = "/assets/images/icon-arrow-right-hover.svg";
+  });
+
+  generateBtn.addEventListener("mouseout", () => {
+    arrowRight.src = "/assets/images/icon-arrow-right.svg";
+  });
+
+  //
 });
